0598f0548d9dba973b11550de94b3c14f4b5cc05
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-12-21 10:15:51 -0300

    feat(server): addition of the database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,12 @@ import express, { Request, Response } from 'express';
 import { configureCORS } from './middlewares/cors-middleware';
 import { errorMiddleware } from './middlewares/error-middleware';
 import { indexRouter } from './routes/index-route';
+const { connectDatabase } = require('./database/db');
 
 const app = express();
 
 app.use(express.json());
+connectDatabase();
 configureCORS(app);
 app.use(errorMiddleware);
 
@@ -13,7 +15,7 @@ app.use('/', indexRouter);
 
 app.use((req: Request, res: Response) => {
     res.status(404).json({
-        message: 'Rota não encontrada.',
+        message: 'Route not found.',
     });
 });
 
